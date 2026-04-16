6c78bb958c7493e32d86bfa92b5c51f6a867d2f4
bensonzhow <xiaoyou91@vip.qq.com>
2026-04-16 23:41:59 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 node red usb key
 
 调用USB加密狗
+
+## 安装
+
+要安装稳定版本，可以使用 `菜单 - 管理调色板 - 安装` 选项并搜索 node-red-zusbdll，或者在你的 Node-RED 用户目录（通常为 `~/.node-red`）下运行以下命令：
+
+  npm i node-red-zusbdll
---
+++ win-x86/SG/test.c
@@ -0,0 +1,63 @@
+﻿#include "SG.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	BOOL ret = 0;
+	char error[32] = {0};
+
+	ret = login(0, error);
+	if (0 == ret)
+	{
+		printf("id(0)测试失败!\n");
+	}
+	else
+	{
+		printf("id(0)测试成功!\n");
+	}
+
+	ret = login(1, error);
+	if (0 == ret)
+	{
+		printf("id(1)测试失败!\n");
+	}
+	else
+	{
+		printf("id(1)测试成功!\n");
+	}
+
+	//ret = login(2, error);
+	//if (0 == ret)
+	//{
+	//	printf("id(2)测试失败!\n");
+	//}
+	//else
+	//{
+	//	printf("id(2)测试成功!\n");
+	//}
+
+	ret = login(3, error);
+	if (0 == ret)
+	{
+		printf("id(3)测试失败!\n");
+	}
+	else
+	{
+		printf("id(3)测试成功!\n");
+	}
+
+	ret = login(4, error);
+	if (0 == ret)
+	{
+		printf("id(4)测试失败!\n");
+	}
+	else
+	{
+		printf("id(4)测试成功!\n");
+	}
+
+	system("pause");
+	return 0;
+}
